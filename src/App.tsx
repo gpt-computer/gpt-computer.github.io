3e0102f2585e9fee5bc6bfdad497ec9c2224b675
xeondesk <xeondesk@gmail.com>
2026-05-05 23:21:11 +0000

    ⚙️ Update App.tsx with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { Home } from "./pages/Home"
+import { Products } from "./pages/Products"
+import { Repositories } from "./pages/Repositories"
+
+const queryClient = new QueryClient()
+
 function App() {
-    return <div></div>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/repositories" element={<Repositories />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  )
 }
 
 export default App
